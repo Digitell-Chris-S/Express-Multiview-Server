67b890c77ebf79511ef9dbbc586bfafc00b67d96
Chris Sampson <65880029+JorganBonstragel@users.noreply.github.com>
2021-06-30 16:27:13 -0400

    remove firebase and lock viewer routes behind login

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,6 +1,11 @@
 const isLoggedIn = (req, res, next) => {
+    console.log(req.originalUrl)
     if(!req.isAuthenticated()){
+        // Dont Flash when user first encounters login
+        if(req.originalUrl != '/viewer'){
         req.flash('error', 'You must be signed in');
+        return res.redirect('/login')
+        }
         return res.redirect('/login')
     }
     next()
--- routes/viewer.js
+++ routes/viewer.js
@@ -6,7 +6,7 @@ const Desktop = require('../models/desktop');
 const Laptop = require('../models/laptop');
 const isLoggedIn = require('../middleware/isLoggedIn');
 
-router.get('/', catchAsync( async (req, res) => {
+router.get('/', isLoggedIn, catchAsync( async (req, res) => {
     const desktops = await Desktop.find({});
     const laptops = await Laptop.find({});
 
